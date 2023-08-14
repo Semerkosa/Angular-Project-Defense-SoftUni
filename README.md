9138b7eae10fa60ca3009a5c88a17f11428528ea
TSemerdzhiev <semerkosa@abv.bg>
2023-08-14 21:16:21 +0300

    empty project structure added

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 The src code of my first Angular project at SoftUni, which I will have to defend a few days from its creation... 
 
-First project idea - book library or something similar to Audible/Storytel.
+The project will be about workout programs. A logged in user will be able to 'buy' a program and/or hire a personal online coach (trainer) and will be able to see the programs he owns and some extra details + read reviews of other users.
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,13 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { AuthModule } from './auth/auth.module';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { WorkoutProgramComponent } from './feature/workout-program/workout-program.component';
+import { TrainersComponent } from './feature/trainers/trainers.component';
+import { CardDetailsComponent } from './feature/card-details/card-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    WorkoutProgramComponent,
+    TrainersComponent,
+    CardDetailsComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    CoreModule.forRoot(),
+    AuthModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
