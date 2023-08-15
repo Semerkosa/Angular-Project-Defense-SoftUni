2a00d13092e09559142828e346101f6c4e6b8f01
Teodor Semerdzhiev <tsemerdzhiev@vmobile.eu>
2023-08-15 14:14:03 +0300

    added login/register html/css

--- project/src/app/auth/auth.module.ts
+++ project/src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { ProfileComponent } from './profile/profile.component';
     ProfileComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
