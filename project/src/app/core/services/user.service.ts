e8aa614b3edd29f63c1bd3af9f0b3fffb25cb015
Teodor Semerdzhiev <tsemerdzhiev@vmobile.eu>
2023-08-17 16:14:17 +0300

    buy program functionality added

--- project/src/app/core/services/user.service.ts
+++ project/src/app/core/services/user.service.ts
@@ -2,48 +2,68 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { ICreateUserDto, ILoginUserDto } from '../interfaces';
+import { ICreateUserDto, ILoginUserDto, IUser, IWorkoutProgram } from '../interfaces';
+
+const serverUrl = `${environment.apiUrl}/users`
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class UserService {
-  private isAuthenticatedSubject = new BehaviorSubject<boolean>(this.isLoggedIn());
-  public isAuthenticated$: Observable<boolean> = this.isAuthenticatedSubject.asObservable();
+	private isAuthenticatedSubject = new BehaviorSubject<boolean>(this.isLoggedIn());
+	public isAuthenticated$: Observable<boolean> = this.isAuthenticatedSubject.asObservable();
 
-  constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient) { }
 
-  updateLoginStatus(status: boolean) {
-    this.isAuthenticatedSubject.next(status);
-  }
+	updateLoginStatus(status: boolean) {
+		this.isAuthenticatedSubject.next(status);
+	}
 
-  isLoggedIn(): boolean {
-    return !!localStorage.getItem("token"); // to retrieve boolean
-  }
+	isLoggedIn(): boolean {
+		return !!localStorage.getItem("token"); // to retrieve boolean
+	}
 
-  getUserFullName(): string {
-    console.log("service called local storage for name ", localStorage.getItem("fullName"));
+	getUserFullName(): string {
+		const fullName = localStorage.getItem("fullName");
+		console.log("userService called for name ", localStorage.getItem("fullName"));
 
-    const fullName = localStorage.getItem("fullName");
+		return fullName ? fullName : "";
+	}
 
-    return fullName ? fullName : "";
-  }
+	getUserId(): string {
+		const userId = localStorage.getItem("id");
+		console.log("userService calledfor user id ", userId);
 
-  login$(user: ILoginUserDto): Observable<any> {
-    const url = `${environment.apiUrl}/login`;
-    return this.http.post<ILoginUserDto>(url, user, environment.httpOptions);
-  }
+		return userId ? userId : "";
+	}
 
-  register$(user: ICreateUserDto): Observable<any> {
-    const url = `${environment.apiUrl}/register`;
-    return this.http.post<ICreateUserDto>(url, user, environment.httpOptions);
-  }
+	getUserById$(id: number): Observable<IUser> {
+		return this.http.get<IUser>(`${serverUrl}/${id}`);
+	}
 
-  logout(): void {
-    localStorage.removeItem("id");
-    localStorage.removeItem("fullName");
-    localStorage.removeItem("token");
+	editUserWorkoutPrograms$(userId: number, programs: IWorkoutProgram[]): Observable<IUser> {
+		const body = {
+			"purchasedWorkoutPrograms": programs
+		}
+		
+		return this.http.patch<IUser>(`${serverUrl}/${userId}`, body, environment.httpOptions);
+	}
 
-    this.updateLoginStatus(false);
-  }
+	login$(user: ILoginUserDto): Observable<any> {
+		const url = `${environment.apiUrl}/login`;
+		return this.http.post<ILoginUserDto>(url, user, environment.httpOptions);
+	}
+
+	register$(user: ICreateUserDto): Observable<any> {
+		const url = `${environment.apiUrl}/register`;
+		return this.http.post<ICreateUserDto>(url, user, environment.httpOptions);
+	}
+
+	logout(): void {
+		localStorage.removeItem("id");
+		localStorage.removeItem("fullName");
+		localStorage.removeItem("token");
+
+		this.updateLoginStatus(false);
+	}
 }
--- project/src/app/feature/workout-programs/workout-programs-list/workout-programs-list.component.ts
+++ project/src/app/feature/workout-programs/workout-programs-list/workout-programs-list.component.ts
@@ -1,38 +1,97 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { IWorkoutProgram } from 'src/app/core/interfaces';
+import { UserService } from 'src/app/core/services/user.service';
 import { WorkoutProgramService } from 'src/app/core/services/workout-program.service';
 
 @Component({
-  selector: 'app-workout-programs-list',
-  templateUrl: './workout-programs-list.component.html',
-  styleUrls: ['./workout-programs-list.component.scss']
+	selector: 'app-workout-programs-list',
+	templateUrl: './workout-programs-list.component.html',
+	styleUrls: ['./workout-programs-list.component.scss']
 })
 export class WorkoutProgramsListComponent implements OnInit, OnDestroy {
 
-  subscription: Subscription;
-  isLoaded = false;
-  workoutPrograms: IWorkoutProgram[];
+	subscription: Subscription;
+	isLoaded = false;
+	workoutPrograms: IWorkoutProgram[];
 
-  constructor(private workoutProgramService: WorkoutProgramService) { }
+	constructor(
+		private workoutProgramService: WorkoutProgramService,
+		private userService: UserService,
+		private router: Router) { }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
+	ngOnDestroy(): void {
+		this.subscription.unsubscribe();
+	}
 
-  ngOnInit(): void {
-    this.subscription = this.workoutProgramService.getWorkoutPrograms$().subscribe({
-      next: (programs) => {
-        this.isLoaded = true;
-        
-        console.log(programs);
-        this.workoutPrograms = programs;
-      },
-      error: (err) => {
-        this.isLoaded = true;
+	ngOnInit(): void {
+		this.subscription = this.workoutProgramService.getWorkoutPrograms$().subscribe({
+			next: (programs) => {
+				this.isLoaded = true;
+
+				console.log(programs);
+				this.workoutPrograms = programs;
+			},
+			error: (err) => {
+				this.isLoaded = true;
 				console.log(err);
 			}
-  });
-  }
+		});
+	}
+
+	purchaseProgram(event: Event, program: IWorkoutProgram) {
+		if (!this.userService.isLoggedIn()) {
+			alert("To buy a program, please login.")
+			this.router.navigate(['/login']);
+			return;
+		}
+
+		const btn = event.target as HTMLElement;
+		const userId = +this.userService.getUserId(); // cast the id to number
+
+		this.userService.getUserById$(userId).subscribe({
+			next: user => {
+				console.log("User by id is", user);
+
+				let userPrograms = user.purchasedWorkoutPrograms;
+				let canPurchase = true;
+
+				for (let p of userPrograms) {
+					if (p.id === program.id) {
+						canPurchase = false;
+						break;
+					}
+				}
+
+				if (canPurchase) {
+					userPrograms.push(program);
+					console.log('New list with programs', userPrograms);
+					
+					this.userService.editUserWorkoutPrograms$(userId, userPrograms).subscribe({
+						next: editedUser => {
+							console.log("Edited user is ", editedUser);
+							
+						},
+						error: err => {
+							console.log(err);
+							
+						}
+					});
+
+					btn.textContent = 'Purchased';
+				} else {
+					btn.textContent = 'Already owned';
+				}
+
+				btn.style.backgroundColor = 'darkGreen';
+				btn.style.color = 'white';
+				btn.setAttribute('disabled', '');
+			},
+			error: err => {
+				console.log(err);
+			}
+		});
+	}
 
 }
